3bdb12fadd4fe85641059b72579eb9016600025d
coder4 <lihy@fenbi.com>
2017-08-21 10:53:05 +0800

    MOD: Add job starter

---
+++ my-sample-job/src/main/java/com/coder4/my/sample/job/MySampleJobStarter.java
@@ -0,0 +1,24 @@
+/**
+ * @(#)MySampleJobStarter.java, Aug 21, 2017.
+ * <p>
+ * Copyright 2017 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.coder4.my.sample.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author coder4
+ */
+@SpringBootApplication
+public class MySampleJobStarter {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(MySampleJobStarter.class);
+        application.setWebEnvironment(false);
+        application.run(args);
+    }
+
+}

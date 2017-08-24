3a23cd9871464cca41dc117b3084d991215485ad
coder4 <lihy@fenbi.com>
2017-08-24 17:39:56 +0800

    MOD: Add mysql datasource config

--- my-sample-server/src/test/java/com/coder4/my/sample/server/SampleApplicationTests.java
+++ my-sample-server/src/test/java/com/coder4/my/sample/server/SampleApplicationTests.java
@@ -2,15 +2,19 @@ package com.coder4.my.sample.server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SampleApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 	}
 
 }
